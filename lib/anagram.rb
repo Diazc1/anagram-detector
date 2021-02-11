bbd795a42fe137237de447dda5fb065d14d3ab2a
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-11 14:00:54 -0800

    commit tree ef7f98471e93b2f64c7e5ac65f70ef17734724e7

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+    
+    def match(word_array)
+        word_array.select do |word|
+            word.split("").sort == @word.split("").sort
+        end
+    end
+end
+
+
+
+#anagram: a word, phrase, or sentence formed from another by rearranging its letters
+# %w(array) is a notation to write an array of strings seperated by spaces instead of commas and without quotes around them
+#  EX: %w(foo bar) is a shortcut for ["foo", "bar"]
+#you can split a word into an array of letters using some_word.split("") you can compare the two arrays using the ==
